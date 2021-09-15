a6937523f01ab2279cdb970e9abe0a883e5c76e4
Melvin <kurianmelvin@users.noreply.github.com>
2021-09-15 12:03:11 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import { Navigation } from "./src/infrastructure/navigation/index";
+
+export default function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
